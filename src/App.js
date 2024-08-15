251b1c4c340b0e4c5b07c7868dfc77a46ba614bd
joaomarcosp-souza <joaomarcosp.sza@gmail.com>
2024-08-14 22:12:43 -0300

    feat: adiciona a pasta da pagina para a dashboard

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/login/Login';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
+import Dashboard from './pages/dashboard/Dashboard';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' Component={Login}></Route>
+          <Route path='/dashboard' Component={Dashboard}></Route>
         </Routes>
       </BrowserRouter>
       <Footer />
---
+++ src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <>
+            <div className="">
+                <div>
+                    <p>Aqui vai o conteudo</p>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Dashboard;
