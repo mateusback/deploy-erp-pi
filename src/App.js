0d4e6626fe422b920c30b7db85e671e261e657b7
GustavoLopes2 <gustavolopesfeitosa1707@gmail.com>
2024-08-14 22:32:01 -0300

    feat: Adicionando o esboço da tela de login

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/login/Login';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
 
 function App() {
   return (
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,10 +1,32 @@
 import React from "react";
 import './Login.css';
+import { Card } from 'primereact/card';
+import { InputText } from 'primereact/inputtext';
+import { Password } from 'primereact/password';
+import { Button } from 'primereact/button';
+
 
 const Login = () => {
     return (
-        <div>
-            <h1>Página inicial do Login</h1>
+        <div className="login-container">
+            <Card title="Login">
+                <div class="field grid">
+                    <label for="login" style={{ width: 100 }} class="col-fixed">Login</label>
+                    <div class="col">
+                        <InputText id="login" type="text" class="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary" />
+                    </div>
+                </div>
+                <div class="field grid">
+                    <label for="password" style={{ width: 100 }} class="col-fixed">Password</label>
+                    <div class="col">
+                        <Password id="password" type="text" class="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary" />
+                    </div>
+                </div>
+                <Button label="Login" disabled onClick={() => window.open("/")} style={{ marginRight: 20 }} />
+                <Button label="Cadastrar" onClick={() => window.open("/cadastre")} style={{ marginRight: 20 }} />
+                <Button label="Recuperar Senha" onClick={() => window.open("/recover-password")} />
+            </Card>
+
         </div>
     );
 }
