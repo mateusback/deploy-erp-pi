d75cf292f40b4a90215cc2bfa7bd40422783355f
Gustavo Lopes Feitosa <103670918+GustavoLopes2@users.noreply.github.com>
2024-08-31 22:27:57 -0300

    feat: Adicionando validação de senha.

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -4,7 +4,6 @@ import { Card } from 'primereact/card';
 import { Button } from 'primereact/button';
 import { Link, useNavigate } from 'react-router-dom';
 
-import Checkbox from '@mui/material/Checkbox';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import TextField from '@mui/material/TextField';
 import IconButton from '@mui/material/IconButton';
@@ -13,11 +12,15 @@ import InputAdornment from '@mui/material/InputAdornment';
 import FormControl from '@mui/material/FormControl';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import { validatePassword } from './../../validations/passwordValidation';
 
 const Login = () => {
 
-    const [checked, setChecked] = React.useState(false);
     const navigate = useNavigate();
+
+    const [password, setPassword] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+    const [isPasswordValid, setIsPasswordValid] = useState(false);
 
     const [showPassword, setShowPassword] = React.useState(false);
     const handleClickShowPassword = () => setShowPassword((show) => !show);
@@ -25,35 +28,33 @@ const Login = () => {
         event.preventDefault();
     };
 
-    const [user, setUser] = useState({ email: "", password: "" });
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
 
-    const handleChange = (input) => {
-        setUser({ ...user, [input.target.name]: input.target.value })
-    }
+    const handleLoginClick = () => {
+        const errorMessage = validatePassword(password);
+        console.log(errorMessage);
+        setPasswordError(errorMessage);
+        const isValid = errorMessage === "";
 
-    const login = () => {
-
-        if (user.email === "teste@gmail.com" && user.password === "123456") {
-            let token = "token do backend"
-            localStorage.setItem("token", token);
-            localStorage.setItem("email", user.email);
-            navigate("/");
+        if (isValid) {
+            setIsPasswordValid(true);
+            window.open("/");
         } else {
-            alert('usuário ou senha incorretos');
+            setIsPasswordValid(false);
         }
-
-    }
-
+    };
+    
     return (
         <Card className="shadow-5 border-round md:w-30rem">
             <div className="text-center mb-5">
                 <img src="" alt="logo" height={50} className="mb-3" />
                 <div className="text-900 text-3xl font-medium mb-3">Entrar</div>
-                <span className="">para continuar com sua conta Leilão</span>
             </div>
             <div id="field-name" className="field">
                 <FormControl fullWidth>
-                    <TextField onChange={handleChange} id="outlined-textarea" name="email" label="E-mail | Usuário" placeholder="Endereço de e-mail ou nome de usuário" required />
+                    <TextField onChange={handlePasswordChange} id="outlined-textarea" name="email" label="E-mail | Usuário" placeholder="Endereço de e-mail ou nome de usuário" required />
                 </FormControl>
             </div>
             <div id="field-password" className="field">
@@ -64,7 +65,7 @@ const Login = () => {
                         placeholder="Senha deve ter no mínimo 6 caracteres"
                         type={showPassword ? 'text' : 'password'}
                         name="password"
-                        onChange={handleChange}
+                        onChange={handlePasswordChange}
                         endAdornment={
                             <InputAdornment position="end">
                                 <IconButton
@@ -79,20 +80,21 @@ const Login = () => {
                         }
                         label="Senha"
                     />
+                    <div className="error-container">
+                            {passwordError && <small className="p-error">{passwordError}</small>}
+                        </div>
                 </FormControl>
             </div>
 
             <div className="flex align-items-center justify-content-between mb-6">
                 <div className="flex align-items-center">
-                    <Checkbox id="rememberme" onChange={e => setChecked(e.checked)} checked={checked} className="mr-2" />
-                    <label htmlFor="rememberme">Lembrar de mim</label>
                 </div>
                 <Link className="font-medium ml-2 no-underline text-blue-500 text-right cursor-pointer" to="/recover-password" target="_blank">Esqueceu a senha?</Link>
             </div>
 
             <div className="mt-5 mb-5">
                 <div className="col">
-                    <Button onClick={login} className="w-full btn-login" label="Continuar" icon="pi pi-user" iconPos="left" severity="secondary" outlined />
+                    <Button onClick={handleLoginClick} className="w-full btn-login" label="Continuar" icon="pi pi-user" iconPos="left" severity="secondary" outlined />
                 </div>
             </div>
         </Card>
