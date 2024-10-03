4fe7b8fd09b70bda060fcd625035078c23ac9218
joaomarcosp-souza <joaomarcosp.sza@gmail.com>
2024-10-02 21:22:05 -0300

    feat: adiciona a tabela para listar os produtos

--- src/pages/dashboard/products/ProductCreation.jsx
+++ src/pages/dashboard/products/ProductCreation.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const ProductCreation = () => {
-
+    return (
+        <div>
+            <p>Add novo produto</p>
+        </div>
+    )
 }
 
 export default ProductCreation;
--- src/pages/dashboard/products/Products.jsx
+++ src/pages/dashboard/products/Products.jsx
@@ -3,15 +3,30 @@ import React from 'react';
 import { Button } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import FilterAltIcon from '@mui/icons-material/FilterAlt';
+import { useNavigate } from 'react-router-dom';
+import ProductTable from './ProductTable';
 
 const Products = () => {
+
+    const navigate = useNavigate();
+
+    const handleCreateProduct = () => {
+        navigate('/new-product')
+    }
+
     return (
-        <div className='products-header'>
-            <p>Produtos</p>
-            <div className='products-buttons'>
-                <Button className='products-button-filter' startIcon={<FilterAltIcon />}>Filtro</Button>
-                <Button className='products-button-create' startIcon={<AddIcon />}>Adicionar Produto</Button>            </div>
-        </div>
+        <>
+            <div className='products-header'>
+                <p>Produtos</p>
+                <div className='products-buttons'>
+                    <Button className='products-button-filter' startIcon={<FilterAltIcon />}>Filtro</Button>
+                    <Button className='products-button-create' startIcon={<AddIcon />} onClick={handleCreateProduct}>Adicionar Produto</Button>            </div>
+            </div>
+
+            <div className='list-container'>
+                <ProductTable />
+            </div>
+        </>
     );
 }
 
