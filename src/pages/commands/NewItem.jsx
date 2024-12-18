f319b6af7762a4be480a2dfe9acfde2e255ef347
Rubens Matheus <y2afepolfxx@gmail.com>
2024-12-18 13:13:00 -0300

    adicionado tela de pagamento a tela new-items

--- src/pages/commands/NewItem.jsx
+++ src/pages/commands/NewItem.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { Card, CardContent, CardMedia, Typography, Grid, TextField, InputAdornment, IconButton, Box, Button, Tooltip, Divider, ButtonGroup } from '@mui/material';
+import { Card, CardContent, CardMedia, Typography, Grid, TextField, InputAdornment, IconButton, Box, Button, Tooltip, Divider, ButtonGroup, Radio, Switch, FormControlLabel} from '@mui/material';
 import { getAllProducts } from "../../services/ProductService";
-import { Search, Delete, Edit, ArrowRightAltOutlined } from "@mui/icons-material";
+import { Search, Delete, Edit, ArrowRightAltOutlined, ArrowLeftOutlined } from "@mui/icons-material";
 import style from './command.module.css'
 
 const ProdutoCard = ({ nome, preco, imagem }) => (
@@ -23,7 +23,7 @@ const ProdutoCard = ({ nome, preco, imagem }) => (
     </Card>
 );
 
-const ResumoComanda = () => (
+const ResumoComanda = ({ isPagamento, handlePagamento, checked, handleSwitchChange }) => (
     <Box className={style.boxTickets}>
         <Typography className={style.tickeTittle} sx={{ mb: 1 }} variant="h6">Comanda 4</Typography>
         <Box className={style.boxTittleComanda}>
@@ -31,14 +31,136 @@ const ResumoComanda = () => (
                 fullWidth
                 disableElevation
                 variant="contained"
-                aria-label="Disabled button group"
+                aria-label="Button group for comanda"
             >
-                <Button className={style.buttonWidth}>Resumo</Button>
-                <Button className={style.buttonWidth} disabled>Pagamento</Button>
+                <Button className={style.buttonWidth} disabled={!isPagamento}>Resumo</Button>
+                <Button className={style.buttonWidth} disabled={isPagamento}>Pagamento</Button>
             </ButtonGroup>
         </Box>
-        <Box className={style.boxItens}>
-            <Grid container alignItems="center" spacing={2}>
+        {isPagamento ? (
+            <Box className={style.boxItens}>
+            <Grid item xs={12}>
+                <Box display="flex" justifyContent="space-between" alignItems="center">
+                    <Typography variant="body2">pagamento parcial:</Typography>
+                    <Typography className="box-pagamento" variant="body2">R$ 48,00</Typography>
+                </Box>
+            </Grid>
+    <Grid container spacing={2}>
+    <Grid item xs={12}>
+        <Box display="flex" alignItems="center" gap={0.5}>
+            <Tooltip title="Selecionar Dinheiro" arrow>
+                <IconButton size="small" color="primary">
+                    <Radio />
+                </IconButton>
+            </Tooltip>
+            <Typography fontWeight="bold" variant="body2" color="textPrimary">
+                dinheiro
+            </Typography>
+            <Typography variant="caption" color="textSecondary">
+                troco para:
+            </Typography>
+            <Grid item xs={12} display="flex" justifyContent="flex-end">
+                <Typography variant="body2" className="box-pagamento">
+                    R$ 0,00
+                </Typography>
+            </Grid>
+        </Box>
+    </Grid>
+    <Grid item xs={12}>
+        <Box display="flex" alignItems="center" gap={0.5}>
+            <Tooltip title="Selecionar Cartão" arrow>
+                <IconButton size="small" color="primary">
+                    <Radio />
+                </IconButton>
+            </Tooltip>
+            <Typography variant="body2" color="textPrimary">
+                cartão
+            </Typography>
+        </Box>
+        <Divider sx={{ marginY: 1.5 }} />
+    </Grid>
+</Grid>
+
+
+    <Grid item xs={12}>
+        <Box display="flex" justifyContent="space-between" alignItems="center" gap={1}>
+            <Typography variant="body2">total consumido:</Typography>
+            <Typography variant="body2">R$ 48,00</Typography>
+        </Box>
+        <Divider sx={{ marginY: 1.5 }} />
+    </Grid>
+
+    <Grid item xs={12}>
+        <Box display="flex" justifyContent="space-between" alignItems="center" gap={2}>
+            <Typography variant="body2">desconto:</Typography>
+            <Box display="flex" alignItems="center" gap={0.5}>
+                <Box className="box-pagamento">
+                    <Typography variant="body2" color="textPrimary">
+                        R$
+                    </Typography>
+                </Box>
+                <Box className="box-pagamento">
+                    <Typography variant="body2" color="textPrimary">
+                        0%
+                    </Typography>
+                </Box>
+            </Box>
+        </Box>
+        <Divider sx={{ marginY: 1.5 }} /> 
+    </Grid>
+
+    <Grid item xs={12}>
+        <Box display="flex" justifyContent="space-between" alignItems="center" gap={1}>
+            <Typography variant="body2">total devido:</Typography>
+            <Typography variant="body2">R$ 48,00</Typography>
+        </Box>
+        <Divider sx={{ marginY: 1.5 }} /> 
+    </Grid>
+
+    <Grid item xs={12}>
+        <Box display="flex" justifyContent="space-between" alignItems="center" gap={1}>
+            <Typography variant="body2">pago:</Typography>
+            <Typography variant="body2">R$ 0,00</Typography>
+        </Box>
+        <Box display="flex" justifyContent="space-between" alignItems="center" gap={1}>
+            <Typography variant="body2">troco:</Typography>
+            <Typography variant="body2">R$ 0,00</Typography>
+        </Box>
+        <Divider sx={{ marginY: 1.5 }} /> 
+    </Grid>
+
+    <Grid item xs={12}>
+        <Box display="flex" justifyContent="space-between" alignItems="center" gap={1} className="box-pagamento" sx={{ marginY: 1.5 }} >
+            <Typography fontWeight="bold" variant="body2">total restante:</Typography>
+            <Typography className={style.tickeTittle} variant="body2">R$ 48,00</Typography>
+        </Box >
+    </Grid >
+
+    <Grid item xs={12}>
+        <Box display="flex" justifyContent="space-between" alignItems="center" gap={1}>
+        <FormControlLabel
+            control={
+              <Switch checked={checked} onChange={handleSwitchChange} size="small" color="primary" sx={{ml:1}}/>
+            }
+            label="concluir ao pagar totalmente"
+          />
+            <Button variant="contained" className={style.buttonSend}>
+                Pagar
+            </Button>
+        </Box>
+        <Divider sx={{ marginY: 1.5 }} />
+    </Grid>
+    <Box className={style.boxButton}>
+            <Button variant="contained" className={style.buttonSend}>
+                <ArrowLeftOutlined />
+                Fechar conta
+            </Button>
+        </Box>
+        </Box>
+        
+        ) : (
+            <Box className={style.boxItens}>
+                <Grid container alignItems="center" spacing={2}>
                 <Grid item xs={1}>
                     <Typography variant="body2" color="#f50057">1x</Typography>
                 </Grid>
@@ -65,8 +187,8 @@ const ResumoComanda = () => (
                     </Box>
                 </Grid>
             </Grid>
-        </Box>
-        <Box className={style.boxInformation}>
+            <Divider sx={{ marginY: 1.5 }} />
+            <Box className={style.boxInformation} sx={{ marginY: 1.5 }} >
             <Typography variant="subtitle1" gutterBottom><strong>Subtotal: </strong>R$ 48,00</Typography>
             <TextField
                 fullWidth
@@ -76,19 +198,29 @@ const ResumoComanda = () => (
                 InputProps={{ style: { color: '#333' } }}
             />
             <Typography className={style.subTittle} gutterBottom>DESTINO DESTE PRODUTO</Typography>
-
-        </Box>
-        <Box className={style.boxButton}>
-            <Button variant="contained" className={style.buttonSend}>
-                Colocar na Comanda
-                <ArrowRightAltOutlined />
-            </Button>
-        </Box>
-    </Box >
+            </Box>
+            </Box>
+        )}
+        {!isPagamento && (
+            <Box className={style.boxButton}>
+                <Button
+                    variant="contained"
+                    className={style.buttonSend}
+                    onClick={handlePagamento}
+                >
+                    Colocar na Comanda
+                    <ArrowRightAltOutlined />
+                </Button>
+            </Box>
+        )}
+    </Box>
 );
+
 
 const NewItem = () => {
     const [produtos, setProdutos] = useState([]);
+    const [isPagamento, setIsPagamento] = useState(false);
+    const [checked, setChecked] = useState(false);
 
     const fetchProducts = async () => {
         try {
@@ -97,6 +229,14 @@ const NewItem = () => {
         } catch (error) {
             console.error('Erro ao buscar produtos:', error);
         }
+    };
+
+    const handleSwitchChange = (event) => {
+        setChecked(event.target.checked);
+      };
+
+    const handlePagamento = () => {
+        setIsPagamento(true); 
     };
 
     useEffect(() => {
@@ -141,9 +281,11 @@ const NewItem = () => {
                     </Grid>
                 </div>
             </Grid>
-            {/* CONTAINER PARA A COMANDA */}
             <Grid item xs={12} md={5} mb={5}>
-                <ResumoComanda />
+                <ResumoComanda
+                 isPagamento={isPagamento} 
+                 handlePagamento={handlePagamento} 
+                  />
             </Grid>
         </Grid>
     );
